c879b8ebf0e078fcea548ebcd436ec09838b0d4c
AdaBan <ada.banaszak@gmail.com>
2024-06-07 11:13:08 +0200

    SEO - meta title, description, default OG image

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Web Web Club';
-export const SITE_DESCRIPTION = 'Blog o HTML i CSS';
+export const SITE_TITLE = "Web Web Club - blog o programowaniu";
+export const SITE_DESCRIPTION =
+  "Dzielę się zajawką na programowanie oraz wiedzą na temat HTML, CSS i szeroko pojętego front-end / web developmentu. Web Web Club to sprawdzone metody optymalizacji kodu, najnowsze standardy i praktyczne wskazówki z pierwszej ręki.";
